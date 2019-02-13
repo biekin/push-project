140d8f9e1675292d1dbd1cb9e347249871a0f99c
agnjasn <agnieszka.jasnosz07@gmail.com>
2019-02-13 18:42:14 +0100

    merged

--- src/server/myCtrl.ts
+++ src/server/myCtrl.ts
@@ -3,12 +3,12 @@ app.controller("myCtrl", ['$scope', '$http', function($scope, $http) {
     $scope.alerts= [{}] ;
 
     const getAlerts = () => $http({method: 'GET', url: 'http://localhost:3000/alerts'}).then(
-        function successCallback(response) {
+        function successCallback(response: { data: any; }) {
             console.log(response)
             $scope.alerts = $scope.alerts.concat(response.data)
             getAlerts();
         },
-        function errorCallback(response) {
+        function errorCallback(response: any) {
             console.log(response);
             getAlerts();
         }
